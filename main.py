29550c22509864f7c6274d1ea244b98280b794fe
王睿 <8114529+ProletariatRR@user.noreply.gitee.com>
2023-03-01 15:03:19 +0800

    FPP class super

--- main.py
+++ main.py
@@ -10,6 +10,96 @@ import pyaudio
 
 # PStatClient.connect() # 占用分析
 
+
+class FPP(FirstPersonController):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+
+    def update(self):
+        self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
+
+        self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[0]
+        self.camera_pivot.rotation_x= clamp(self.camera_pivot.rotation_x, -90, 90)
+
+        self.direction = Vec3(
+            self.forward * (held_keys['w'] - held_keys['s'])
+            + self.right * (held_keys['d'] - held_keys['a'])
+            ).normalized()
+
+        feet_ray = raycast(self.position+Vec3(0,0.5,0), self.direction, ignore=(self,), distance=.5, debug=False)
+        head_ray = raycast(self.position+Vec3(0,self.height-.1,0), self.direction, ignore=(self,), distance=.5, debug=False)
+        if not feet_ray.hit and not head_ray.hit:
+            move_amount = self.direction * time.dt * self.speed
+
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(1,0,0), distance=.5, ignore=(self,)).hit:
+                move_amount[0] = min(move_amount[0], 0)
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(-1,0,0), distance=.5, ignore=(self,)).hit:
+                move_amount[0] = max(move_amount[0], 0)
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(0,0,1), distance=.5, ignore=(self,)).hit:
+                move_amount[2] = min(move_amount[2], 0)
+            if raycast(self.position+Vec3(-.0,1,0), Vec3(0,0,-1), distance=.5, ignore=(self,)).hit:
+                move_amount[2] = max(move_amount[2], 0)
+            self.position += move_amount
+
+            # self.position += self.direction * self.speed * time.dt
+
+
+        if self.gravity:
+            # gravity
+            ray = raycast(self.world_position+(0,self.height,0), self.down, ignore=(self,))
+            # ray = boxcast(self.world_position+(0,2,0), self.down, ignore=(self,))
+
+            if ray.distance <= self.height+.1:
+                if not self.grounded:
+                    self.land()
+                self.grounded = True
+                # make sure it's not a wall and that the point is not too far up
+                if ray.world_normal.y > .7 and ray.world_point.y - self.world_y < .5: # walk up slope
+                    self.y = ray.world_point[1]
+                return
+            else:
+                self.grounded = False
+
+            # if not on ground and not on way up in jump, fall
+            self.y -= min(self.air_time, ray.distance-.05) * time.dt * 100
+            self.air_time += time.dt * .25 * self.gravity
+
+
+    def input(self, key):
+        if key == 'space':
+            self.jump()
+
+
+    def jump(self):
+        if not self.grounded:
+            return
+
+        self.grounded = False
+        self.animate_y(self.y+self.jump_height, self.jump_up_duration, resolution=int(1//time.dt), curve=curve.out_expo)
+        invoke(self.start_fall, delay=self.fall_after)
+
+
+    def start_fall(self):
+        self.y_animator.pause()
+        self.jumping = False
+
+    def land(self):
+        # print('land')
+        self.air_time = 0
+        self.grounded = True
+
+
+    def on_enable(self):
+        mouse.locked = True
+        self.cursor.enabled = True
+
+
+    def on_disable(self):
+        mouse.locked = False
+        self.cursor.enabled = False
+
+
 app = Ursina()
 
 Entity.default_shader = lit_with_shadows_shader
@@ -18,7 +108,7 @@ sun = DirectionalLight()
 sun.look_at(Vec3(1,-1,-1))
 Sky()
 
-player = FirstPersonController(model='cube', color=color.white66, speed=8)
+player = FPP(model='cube', color=color.white66, speed=8)
 max_fps = 60
 dtime = 1/max_fps
 
@@ -61,7 +151,7 @@ stream = p.open(
                     rate=44100,
                     output=True,
                     input=True,
-                    frames_per_buffer=1024,
+                    frames_per_buffer=32e,
                     stream_callback=callback
                 )
 
